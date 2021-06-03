34e9c09b1e95b9715625bf273861a4c1926dc260
htlsne <htlsne23@gmail.com>
2021-06-04 00:00:16 +0900

    Update

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 import { chromium } from 'playwright';
 
 (async () => {
-  const browser = await chromium.launch({ headless: false, slowMo: 50 });
+  const browser = await chromium.launch({ headless: false, slowMo: 500 });
+
   const page = await browser.newPage();
-  await page.goto('http://whatsmyuseragent.org/');
-  await page.screenshot({ path: `example.png` });
+  await page.goto('http://localhost:3000/');
+  const content = await page.textContent('.MuiTableBody-root > tr:nth-child(1) > td:nth-child(1)');
+  console.log(content);
+  await page.click('[aria-label="Next Page"]');
+
   await browser.close();
 })();
